7979feaa7769dcb6495e24d4a8bd420840e45b9c
zyfhaha <707314696@qq.com>
2019-11-08 13:29:44 -0500

    code

--- src/Map.py
+++ src/Map.py
@@ -7,16 +7,28 @@ class Map:
         x = 0
         with open(file, 'r') as f:
             for line in f:
-                stateline=list([])
+                stateline = list([])
                 p = list(line.split(' '))
                 for y in range(len(p)):
-                    st = State(x,y,int(p[y]))
+                    st = State(x, y, int(p[y]))
                     stateline.append(st)
                 self.states.append(stateline)
-                x+=1
+                x += 1
+        self.sizex = len(self.states)
+        self.sizey = len(self.states[0])
+        for i in range(len(self.states)):
+            for j in range(len(self.states[i])):
+                if i == 0 or self.states[i - 1][j].type == 1:
+                    self.states[i][j].moveable[0] = 0
+                if j == 0 or self.states[i][j - 1].type == 1:
+                    self.states[i][j].moveable[2] = 0
+                if i == self.sizex - 1 or self.states[i + 1][j].type == 1:
+                    self.states[i][j].moveable[1] = 0
+                if j == self.sizey - 1 or self.states[i][j + 1].type == 1:
+                    self.states[i][j].moveable[3] = 0
 
     def mapprint(self):
         for i in range(len(self.states)):
             for j in range(len(self.states[i])):
-                print(self.states[i][j].type,end=' ')
+                print(self.states[i][j].type, end=' ')
             print()
--- src/State.py
+++ src/State.py
@@ -3,4 +3,5 @@ class State:
         self.locationx=x
         self.locationy=y
         self.q=[50,50,50,50]
+        self.moveable=[1,1,1,1]
         self.type=t
