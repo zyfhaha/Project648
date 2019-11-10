0683df9af69b0be0396ca16050db667c6782391d
zyfhaha <707314696@qq.com>
2019-11-09 23:05:08 -0500

    code

--- src/main.py
+++ src/main.py
@@ -1,28 +1,35 @@
 import random
 from Map import Map
 
-
-map=Map('data.txt')
-goalx=5
-goaly=7
-startx=2
-starty=0
-moveoption1=[-1,+1,0,0]
-moveoption2=[0,0,-1,+1]
-k=0
-e=0.1
+map = Map('data.txt')
+goalx = 5
+goaly = 7
+startx = 2
+starty = 0
+moveoption1 = [-1, +1, 0, 0]
+moveoption2 = [0, 0, -1, +1]
+k = 0
+e = 0.1
+alpha=1
+gama=0.9
 map.mapprint()
-while k<200:
+while k < 200:
     sx = startx
     sy = starty
     s = map.states[sx][sy]
-    searchlist=[]
-    actionlist=[]
-    while map.isterminal(s)==0:
-        searchlist.append(s)
-        ra=random.random()
-        mvablist=[]
+    while map.isterminal(s) == 0:
+        ra = random.random()
+        mvablist = []
         for i in range(4):
-            if s.moveable[i]==1:
+            if s.moveable[i] == 1:
                 mvablist.append(i)
-        if ra>e:
+        if ra > e:
+            maxi = 0
+            for ii in range(len(mvablist)):
+                if s.q[mvablist[ii]] > s.q[mvablist[maxi]]:
+                    maxi = ii
+            maxm = mvablist[maxi]
+            s1 = map.states[s.locationx + moveoption1[maxm]][s.locationy + moveoption2[maxm]]
+        else:
+            maxm = mvablist[random.randint(0, len(mvablist) - 1)]
+            s1 = map.states[s.locationx + moveoption1[maxm]][s.locationy + moveoption2[maxm]]
